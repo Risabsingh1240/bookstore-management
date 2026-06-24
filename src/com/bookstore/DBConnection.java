04e673c91c0297429d2b63852c7ee95ea03126c6
RISAB KUMAR <singhrisab95@gmail.com>
2026-06-25 01:48:48 +0530

    Update DBConnection to use an empty password
    Changed the database password to an empty string.

--- src/com/bookstore/DBConnection.java
+++ src/com/bookstore/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/bookstore_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "1240";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         Connection con = null;
